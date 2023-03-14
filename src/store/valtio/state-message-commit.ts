fc9321097965b032a94b7a602ff013220b0f4a67
Willian David Calin <david2c142@gmail.com>
2023-03-13 23:46:44 -0500

    feat: add new event source state for better tracking of events

--- src/store/valtio/state-message-commit.ts
+++ src/store/valtio/state-message-commit.ts
@@ -24,34 +24,29 @@ export const setMessageEntryCommit = async (
   const { infinitiveVerbCommit, objectCommit, descriptionCommit } =
     storeMessageEntryCommit.entryMessageCommit;
 
-  // const outputMessage = await servicesCommit({
-  //   infinitiveVerbCommit,
-  //   objectCommit,
-  //   descriptionCommit,
-  // });
   const url = `${API_URL}/${infinitiveVerbCommit}/${objectCommit}/${descriptionCommit}`;
-
+  console.log(url);
   const eventSource = new EventSource(url);
+  let message = "";
+  // console.log(eventSource);
 
-  console.log(eventSource);
+  eventSource.onerror = (error) => {
+    console.error("Event source Onerror:", error);
+    eventSource.close();
+    storeMessageEntryCommit.streaming = false;
+  };
 
-  // eventSource.onerror = (error) => {
-  //   console.error("Event source 1:", error);
-  //   eventSource.close();
-  //   storeMessageEntryCommit.streaming = false;
-  // };
+  eventSource.onmessage = (event) => {
+    // console.error("Event source event:", event);
+    const { data } = event;
+    console.log(data);
+    if (data === "[DONE]") {
+      storeMessageEntryCommit.streaming = false;
 
-  // eventSource.onmessage = (event) => {
-  //   const { data } = event;
-
-  //   if (data === "[DONE]") {
-  //     storeMessageEntryCommit.streaming = false;
-
-  //     eventSource.close();
-  //     return;
-  //   }
-  //   console.log("Hello");
-
-  //   storeMessageEntryCommit.outputMessageCommit = JSON.parse(data);
-  // };
+      eventSource.close();
+      return;
+    }
+    message += JSON.parse(data);
+    storeMessageEntryCommit.outputMessageCommit = message;
+  };
 };
